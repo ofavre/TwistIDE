a4de440c3c3f4e291bc392dfa843a152838902ba
Dan Forest-Barbier <dan@twisted-coders.net>
2012-04-14 23:19:17 +0200

    Added a dependency downloader script and a .gitignore

---
+++ get_llvm.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+BASE=http://llvm.org/svn/llvm-project
+
+echo "Downloading TwistIDE dependencies from the LLVM SVN repository to ./llvm"
+echo ""
+echo "Checking out LLVM (trunk) ..."
+time -f "      Done in %E" svn co --force -q $BASE/llvm/trunk llvm
+
+echo "Checking out LLDB (trunk) ..."
+time -f "      Done in %E" svn co --force -q $BASE/lldb/trunk llvm/tools/lldb
+
+echo "Checking out Clang (trunk) ..."
+time -f "      Done in %E" svn co --force -q $BASE/cfe/trunk llvm/tools/clang
+
+echo "Checking out Compiler-RT (trunk) ..."
+time -f "      Done in %E" svn co --force -q $BASE/compiler-rt/trunk llvm/projects/compiler-rt
+
+echo "Extracting revision numbers from LLDB scripts ..."
+LLVM_REV=`grep -m 1 llvm_revision llvm/tools/lldb/scripts/build-llvm.pl | sed "s/[^0-9]*\([0-9]*\)[^0-9]*/\1/"`
+CLANG_REV=`grep -m 1 clang_revision llvm/tools/lldb/scripts/build-llvm.pl | sed "s/[^0-9]*\([0-9]*\)[^0-9]*/\1/"`
+
+echo "Updating LLVM to r$LLVM_REV ..."
+time -f "      Done in %E" svn update --force -q -r $LLVM_REV llvm
+
+echo "Updating Clang to r$CLANG_REV ..."
+time -f "      Done in %E" svn update --force -q -r $CLANG_REV llvm/tools/clang
+
