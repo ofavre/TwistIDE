dcde9640647414b369ee030af0db3064129840a9
Olivier Favre <of.olivier.favre@gmail.com>
2012-07-24 23:07:55 +0200

    Using Driver to build actions and set includes
    Driver recognises the host target, and manages system includes.
    
    I instructed it to do preprocessing only (CPP=1).
    I print the constructed Actions.
    
    For now I left the ExecuteCompilation call, which calls the same program
    as a subprocess, with -cc1 and every necessary argument (like include
    dirs). But it doesn't quite work.
    Anyway, we don't want to make a compiler here, like in
    llvm/tools/clang/tools/driver/driver.cpp.
    We merely want to do proper initialization, and some preprocessing.

--- tests/amnoid-clang-tutorial/src/inputargs.cpp
+++ tests/amnoid-clang-tutorial/src/inputargs.cpp
@@ -6,29 +6,35 @@ using namespace std;
 #include <llvm/Config/config.h>
 #include <llvm/Support/Host.h>
 
-#include <clang/Basic/TargetInfo.h>
 #include <clang/Basic/FileManager.h>
 #include <clang/Driver/ArgList.h>
+#include <clang/Driver/Arg.h>
+#include <clang/Driver/Driver.h>
+#include <clang/Driver/Compilation.h>
+#include <clang/Driver/Action.h>
 #include <clang/Lex/Preprocessor.h>
 #include <clang/Frontend/FrontendActions.h>
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/CompilerInvocation.h>
-#include <clang/Frontend/DiagnosticOptions.h>
-#include <clang/Frontend/TextDiagnosticPrinter.h>
 #include <clang/Frontend/Utils.h>
 using namespace clang;
+using llvm::dyn_cast;
+using clang::driver::Arg;
 using clang::driver::InputArgList;
+using clang::driver::DerivedArgList;
 using clang::CompilerInstance;
-using clang::DiagnosticOptions;
-using clang::TextDiagnosticPrinter;
-using clang::DiagnosticConsumer;
-using clang::DiagnosticsEngine;
-using clang::TargetInfo;
 using llvm::sys::getDefaultTargetTriple;
-using clang::PrintPreprocessedAction;
+using llvm::sys::Path;
+using llvm::ArrayRef;
+using clang::driver::Driver;
+using clang::driver::Compilation;
+using clang::driver::Command;
+using clang::driver::ActionList;
+using clang::driver::Action;
+using clang::driver::InputAction;
 
 
-static const char *getInputKindName(InputKind Kind);
+static llvm::sys::Path GetExecutablePath(const char *Argv0, bool CanonicalPrefixes);
 
 
 int main(int argc, char* argv[])
@@ -41,36 +47,83 @@ int main(int argc, char* argv[])
 
   CompilerInstance ci;
   ci.createDiagnostics(argc, argv + 1);
-  CompilerInvocation::CreateFromArgs(ci.getInvocation(), argv + 1, argv + argc, ci.getDiagnostics());
 
-  vector<FrontendInputFile>& inputs = ci.getFrontendOpts().Inputs;
-  cout << "Input files: (" << inputs.size() << ")" << endl;
-  for (vector<FrontendInputFile>::const_iterator it = inputs.begin(), end = inputs.end() ; it != end ; ++it)
-    cout << " - type:" << getInputKindName(it->Kind) << "\tfrom:" << (it->IsSystem ? "system" : "user") << "\tname:\"" << it->File << "\"" << endl;
+  Path path = GetExecutablePath(argv[0], true);
+  Driver driver (path.str(), getDefaultTargetTriple(), "a.out", true, ci.getDiagnostics());
+  driver.CCCIsCPP = true; // only preprocess
+  OwningPtr<Compilation> compil (driver.BuildCompilation(llvm::ArrayRef<const char*>(argv, argc)));
+  if (!compil) {
+    cerr << "Could not build a Compilation!" << endl;
+    return EXIT_FAILURE;
+  }
 
-  PrintPreprocessedAction printPpAction;
-  ci.ExecuteAction(printPpAction);
+  driver.PrintActions(*compil);
+
+  // The following list some unknown arguments (like -###) as inputs...
+  //
+  // {
+  //   const InputArgList& inputs = compil->getInputArgs();
+  //   cout << "Input files: (" << inputs.getNumInputArgStrings() << ")" << endl;
+  //   for (unsigned i = 0, n = inputs.getNumInputArgStrings() ; i < n ; ++i)
+  //     cout << " - " << inputs.getArgString(i) << endl;
+  // }
+  //
+  // {
+  //   const DerivedArgList& inputs = compil->getArgs();
+  //   cout << "Input files: (" << inputs.getNumInputArgStrings() << ")" << endl;
+  //   for (unsigned i = 0, n = inputs.getNumInputArgStrings() ; i < n ; ++i)
+  //     cout << " - " << inputs.getArgString(i) << endl;
+  // }
+
+  {
+    const ActionList& actions = compil->getActions();
+    cout << "Actions: (" << actions.size() << ")" << endl;
+    for (ActionList::const_iterator it = actions.begin(), end = actions.end() ; it != end ; ++it) {
+      const Action& a = *(*it);
+      cout << " - " << a.getClassName() << " inputs(" << a.getInputs().size() << ") -> " << clang::driver::types::getTypeName(a.getType()) << endl;
+      for (ActionList::const_iterator it2 = a.begin(), end2 = a.end() ; it2 != end2 ; ++it2) {
+        const Action& a2 = *(*it2);
+        const InputAction* ia = dyn_cast<const InputAction>(&a2);
+        if (ia) {
+          cout << "    - " << a2.getClassName() << " {";
+          const Arg& inputs = ia->getInputArg();
+          for (unsigned i = 0, n = inputs.getNumValues() ; i < n ; ++i) {
+            cout << "\"" << inputs.getValue(compil->getArgs(), i) << "\"";
+            if (i+1 < n) cout << ", ";
+          }
+          cout << "} -> " << clang::driver::types::getTypeName(a2.getType()) << endl;
+        } else
+          cout << "    - " << a2.getClassName() << " inputs(" << a2.getInputs().size() << ") -> " << clang::driver::types::getTypeName(a2.getType()) << endl;
+      }
+    }
+  }
+
+  cout << endl;
+
+  //!\\ The following doesn't quite work
+  //!\\ Tip: Use -### to print commands instead of executing them.
+  //!\\ ExecuteCompilation exec()s the this same program, with extra arguments.
+  //!\\ We should take advantage of the parsing done, and do the preprocess ourselves.
+
+  int Res = 0;
+  const Command *FailingCommand = 0;
+  Res = driver.ExecuteCompilation(*compil, FailingCommand);
+
+  // If result status is < 0, then the driver command signalled an error.
+  // In this case, generate additional diagnostic information if possible.
+  if (Res < 0)
+    driver.generateCompilationDiagnostics(*compil, FailingCommand);
 
   return EXIT_SUCCESS;
 }
 
-static const char *getInputKindName(InputKind Kind) {
-  switch (Kind) {
-  case IK_None:              break;
-  case IK_AST:               return "ast";
-  case IK_Asm:               return "assembler-with-cpp";
-  case IK_C:                 return "c";
-  case IK_CXX:               return "c++";
-  case IK_LLVM_IR:           return "ir";
-  case IK_ObjC:              return "objective-c";
-  case IK_ObjCXX:            return "objective-c++";
-  case IK_OpenCL:            return "cl";
-  case IK_CUDA:              return "cuda";
-  case IK_PreprocessedC:     return "cpp-output";
-  case IK_PreprocessedCXX:   return "c++-cpp-output";
-  case IK_PreprocessedObjC:  return "objective-c-cpp-output";
-  case IK_PreprocessedObjCXX:return "objective-c++-cpp-output";
-  }
+/// Turns argv[0] into the executable path
+llvm::sys::Path GetExecutablePath(const char *Argv0, bool CanonicalPrefixes)
+{
+  if (!CanonicalPrefixes)
+    return llvm::sys::Path(Argv0);
 
-  llvm_unreachable("Unexpected language kind!");
+  // symbol just needs to be some symbol in the binary
+  void *symbol = (void*) (intptr_t) GetExecutablePath; // can't take ::main() address, use the current function instead
+  return llvm::sys::Path::GetMainExecutable(Argv0, symbol);
 }
